961b602ccb30ac64d2adea1250f18c36874e2bdb
Joe Moran <joemoran231@gmail.com>
2019-10-11 07:18:38 -0400

    Did it

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,79 @@
+// TODO: animate through list of images if mobile and tapping
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    {{ mouseY}}
+    <div v-for="(image, index) in images" :key="index">
+      <MeImage v-show="index == section" :image-source="image" />
+    </div>
   </div>
 </template>
 
 <script>
+import MeImage from "./MeImage";
+const imageCount = 17;
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  components: {
+    MeImage
+  },
+  data() {
+    return {
+      win: window,
+      doc: document,
+      docElem: document.documentElement,
+      body: document.getElementsByTagName("body")[0],
+      mouseY: 0,
+      images: [
+        "album-art-0.png",
+        "album-art-1.png",
+        "album-art-2.png",
+        "album-art-3.png",
+        "album-art-4.png",
+        "album-art-5.png",
+        "album-art-6.png",
+        "album-art-7.png",
+        "album-art-8.png",
+        "album-art-9.png",
+        "album-art-10.png",
+        "album-art-11.png",
+        "album-art-12.png",
+        "album-art-13.png",
+        "album-art-14.png",
+        "album-art-15.png",
+        "album-art-16.png"
+      ]
+    };
+  },
+  computed: {
+    x: function() {
+      return (
+        this.win.innerWidth || this.docElem.clientWidth || this.body.clientWidth
+      );
+    },
+    y: function() {
+      return (
+        this.win.innerHeight ||
+        this.docElem.clientHeight ||
+        this.body.clientHeight
+      );
+    },
+    sectionHeight: function() {
+      return this.y / imageCount;
+    },
+    section: function() {
+      return Math.floor(this.mouseY / this.sectionHeight);
+    }
+  },
+  methods: {
+    mouseIsMoving(e) {
+      this.mouseY = e.pageY;
+    }
+  },
+  mounted() {
+    window.addEventListener("mousemove", this.mouseIsMoving);
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/MeImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <figure>
+    <img :src="require('../assets/' + imageSource)" />
+  </figure>
+</template>
+
+<script>
+export default {
+  name: "MeImage",
+  props: {
+    imageSource: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+img {
+  height: 550px;
+}
+</style>
