844ecf930163266e5c341562ee0a7d517e8e26e6
Joe Moran <joemoran231@gmail.com>
2021-06-26 12:30:09 -0400

    Updates constant. Adds comments to code.

--- src/components/Button/BasicButton.vue
+++ src/components/Button/BasicButton.vue
@@ -2,16 +2,16 @@
   <button>
     <SVGIcon
       :icon="prependIcon"
-      :height="$options.iconDimension"
-      :width="$options.iconDimension"
+      :height="$options.ICON_SQUARE_DIMENSION"
+      :width="$options.ICON_SQUARE_DIMENSION"
       v-if="prependIcon"
       class="icon"
     />
     <span id="basic-button-text"><slot></slot></span>
     <SVGIcon
       :icon="appendIcon"
-      :height="$options.iconDimension"
-      :width="$options.iconDimension"
+      :height="$options.ICON_SQUARE_DIMENSION"
+      :width="$options.ICON_SQUARE_DIMENSION"
       v-if="appendIcon"
       class="icon"
     />
@@ -21,18 +21,31 @@
 <script>
 import SVGIcon from "../icons/SVGIcon.vue";
 
+/**
+ * The value to use for the square dimensions of the container of the button icon.
+ */
+const ICON_SQUARE_DIMENSION = 32;
+
+/**
+ * A very basic button, styled specifically for this site.
+ */
 export default {
   name: "BasicButton",
   components: {
     SVGIcon,
   },
   props: {
+    /**
+     * The path of the icon to prepend the button label text, if desired.
+     */
     prependIcon: { type: String, required: false },
+
+    /**
+     * The path of the icon to append to the button label text, if desired.
+     */
     appendIcon: { type: String, required: false },
-    prependColor: { type: String, required: false },
-    appendColor: { type: String, required: false },
   },
-  iconDimension: 32,
+  ICON_SQUARE_DIMENSION,
 };
 </script>
 
