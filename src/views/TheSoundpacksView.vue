0f5dcf7c927101e3160dc07f1c57fcd7d5d6efa4
Joe Moran <joemoran231@gmail.com>
2022-01-17 10:55:13 -0500

    Scaffolds soundpacks view w/ route

---
+++ src/views/TheSoundpacksView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="the-soundpacks-view" />
+</template>
+
+<script>
+/**
+ * The soundpacks view, where users can download soundpacks.
+ */
+export default {
+  name: 'TheSoundpacksView',
+  components: {},
+  props: {},
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
