3450871954fc12ced84046aaf0eff31c58b4f370
Joe Moran <joemoran231@gmail.com>
2022-01-17 10:55:13 -0500

    Scaffolds soundpacks view w/ route

---
+++ src/views/__tests__/the-soundpacks-view.test.js
@@ -0,0 +1,13 @@
+// import { render } from '@testing-library/vue';
+// import { byRole, byText } from 'testing-library-selector';
+// import userEvent from '@testing-library/user-event';
+// import TheSoundpacksView from '@/views/TheSoundpacksView.vue';
+
+// const ui = {
+//   // place reusable ui queries here via testing-library-selector
+// };
+
+/** @TODO */
+it.skip('Does something', () => {
+
+});
