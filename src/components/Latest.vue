0626273ab5a5cccedd15a08fe89335e182eeb52e
Joe Moran <joemoran231@gmail.com>
2024-08-28 22:46:02 -0400

    Add latest single to "Latest" section

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -1,41 +1,6 @@
 <template>
   <div class="latest">
-    <iframe
-      width="100%"
-      height="450"
-      scrolling="no"
-      frameborder="no"
-      allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/621321513&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-    />
-    <div
-      style="
-        font-size: 10px;
-        color: #cccccc;
-        line-break: anywhere;
-        word-break: normal;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        font-family: Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans,
-          Garuda, Verdana, Tahoma, sans-serif;
-        font-weight: 100;
-      "
-    >
-      <a
-        href="https://soundcloud.com/xrgbmusic"
-        title="xRGB"
-        target="_blank"
-        style="color: #cccccc; text-decoration: none"
-      >xRGB</a>
-      ·
-      <a
-        href="https://soundcloud.com/xrgbmusic/sets/refraction"
-        title="Refraction"
-        target="_blank"
-        style="color: #cccccc; text-decoration: none"
-      >Refraction</a>
-    </div>
+   <iframe style="border: 0; width: 426px; height: 518px;" src="https://bandcamp.com/EmbeddedPlayer/track=1548797469/size=large/bgcol=ffffff/linkcol=7137dc/tracklist=false/transparent=true/" seamless><a href="https://xrgb.bandcamp.com/track/prequinox-drive">Prequinox Drive by xRGB</a></iframe>
   </div>
 </template>
 
